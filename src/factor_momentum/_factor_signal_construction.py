17d5fc1aff3fcf5223b495c17065eea159ab275a
rvance1 <roystonvance@gmail.com>
2025-10-20 10:47:16 -0600

    added rolling dicrete

--- src/factor_momentum/_factor_signal_construction.py
+++ src/factor_momentum/_factor_signal_construction.py
@@ -7,13 +7,13 @@ def construct_factor_signal_monthly (
         monthly_factor_returns: pl.LazyFrame, type: str 
 ) -> pl.DataFrame:
 
-    if type == "discrete":
+    if type == "cross section":
         return (monthly_factor_returns.with_columns(
             pl.col("lag_ret").rank('dense').over('month').alias('rank'),
             pl.col('lag_ret').count().over('month').alias('count')
         )
         .with_columns(
-            pl.when(pl.col('rank') < pl.col('count')*0.5)
+            pl.when(pl.col('rank') <= pl.col('count')*0.5)
             .then(-1)
             .otherwise(1)
             .alias('signal')
@@ -21,15 +21,32 @@ def construct_factor_signal_monthly (
         .collect()
         )
     
-    elif type == "rolling":
+    elif type == "rolling continuous":
         return (monthly_factor_returns.with_columns(
-            (np.log(pl.col('lag_ret')*.01+1))
-            .shift(2)
-            .rolling_sum(10)
+            (pl.col('lag_ret'))
+            .shift(1)
+            .rolling_sum(11)
+            .over('factor')
             .alias('signal')
         )
         .drop_nulls()
         .collect()
         )
+    
+    elif type == "rolling discrete":
+        return (monthly_factor_returns.with_columns(
+            (pl.col('lag_ret'))
+            .shift(1)
+            .rolling_sum(11)
+            .over('factor')
+            .alias('signal')
+        )
+        .with_columns(
+            (pl.col('signal') / pl.col('signal').abs()).alias('signal')
+        )
+        .drop_nulls()
+        .collect()
+        )
+    
     else:
-        raise ValueError(f"Invalid type: {type!r}. Must be 'rolling' or 'discrete'.")
+        raise ValueError(f"Invalid type: {type!r}. Must be 'cross section', 'rolling continuous', or 'rolling discrete'.")
